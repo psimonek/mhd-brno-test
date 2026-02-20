97d2989d9d5c7708eefd3c7994d5c9c0d0d704b9
psimonek <kopyta_strelby_0u@icloud.com>
2026-02-20 10:05:22 +0100

    Testing vehicles position

--- js/vehicle-position.js
+++ js/vehicle-position.js
@@ -117,13 +117,15 @@ function createCircleIcon(bearing, size=36, fillColor='#1978c8', lineText='') {
 function updateMarkerIcon(marker, bearing, lineId, lineName, isInactive, vtype, size=36) {
     const color = colorForLine(lineId, vtype, isInactive);
     const txt = (lineName !== undefined && lineName !== null) ? String(lineName) : '';
-    let newIcon;
+
     if (!isInactive) {
         const newIcon = createTriangleIcon(bearing, size, color, txt);
+        marker.setIcon(newIcon);
     } else {
-        const newIcon = createCircleIcon(bearing, size, color, txt);
+        const newIconCircle = createCircleIcon(bearing, size, color, txt);
+        marker.setIcon(newIconCircle);
     }
-    marker.setIcon(newIcon);
+    //marker.setIcon(newIcon);
 }
 
 // --- Zpracování dat (stejné jako dříve) ---
