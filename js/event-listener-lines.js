47c9ecab5183ce530d08558e73efb2b2319f9cac
psimonek <kopyta_strelby_0u@icloud.com>
2026-02-21 17:48:30 +0100

    Testing vehicles position

--- js/event-listener-lines.js
+++ js/event-listener-lines.js
@@ -3,5 +3,6 @@ document.querySelectorAll('.group-content a').forEach(function(link) {
         event.preventDefault(); // Zabráníme výchozímu chování odkazu
         linkaCislo = this.getAttribute('data-line'); // Získáme číslo linky
         loadLinesAndStops(linkaCislo); // Vyvoláme funkci s číslem linky
+        console.log(linkaCislo);
     	});
 });
